a655ff6fe0f17bca40c7de1b5f0b2ae2dbac9d62
Katy La Tour <klatour324@gmail.com>
2021-04-23 18:57:52 -0500

    Add coverage to .gitignore and create service spec for mapquest

---
+++ app/services/mapquest_service.rb
@@ -0,0 +1,14 @@
+class MapquestService
+  def self.conn
+    Faraday.new(url: 'https://www.mapquestapi.com') do |req|
+      req.params['key'] = ENV['MAPQUEST_KEY']
+    end
+  end
+
+  def self.location(location)
+    response = conn.get('/geocoding/v1/address') do |req|
+      req.params['location'] = location
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
