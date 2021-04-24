9e598fae36b696e989d59ae8a9bc0c14bc9b795b
Katy La Tour <klatour324@gmail.com>
2021-04-24 13:40:22 -0500

    Add ForecastController

---
+++ app/controllers/api/v1/forecast_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::ForecastController < ApplicationController
+  def index
+    location = params[:location]
+    map_data = MapquestService.find_location(location)[:results].first
+    coordinates = Coordinate.new(map_data)
+    weather = WeatherService.find_forecast_for_location(coordinates.lat, coordinates.long)
+    current_weather = CurrentWeather.new(weather[:current])
+    require "pry"; binding.pry
+  end
+end
