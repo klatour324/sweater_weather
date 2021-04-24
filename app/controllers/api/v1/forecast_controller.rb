936907255a8aa6936fc749d5b91fa73a6d18ca48
Katy La Tour <klatour324@gmail.com>
2021-04-24 14:37:06 -0500

    Update index action with hourly forecast

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -6,6 +6,6 @@ class Api::V1::ForecastController < ApplicationController
     weather = WeatherService.find_forecast_for_location(coordinates.lat, coordinates.long)
     current_weather = CurrentWeather.new(weather[:current])
     daily_weather = DailyWeather.new(weather[:daily])
-    require "pry"; binding.pry
+    hourly_weather = HourlyWeather.new(weather[:hourly])
   end
 end
