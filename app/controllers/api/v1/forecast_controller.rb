fa4fbda6a02beee219b6d2f4228f1fb71cc05ab6
Katy La Tour <klatour324@gmail.com>
2021-04-24 14:15:39 -0500

    Update index action with daily_weather

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -5,6 +5,7 @@ class Api::V1::ForecastController < ApplicationController
     coordinates = Coordinate.new(map_data)
     weather = WeatherService.find_forecast_for_location(coordinates.lat, coordinates.long)
     current_weather = CurrentWeather.new(weather[:current])
+    daily_weather = DailyWeather.new(weather[:daily])
     require "pry"; binding.pry
   end
 end
