f80c345fa663eb23a33ac034a5a01f9b2b56790e
Katy La Tour <klatour324@gmail.com>
2021-04-25 16:24:59 -0500

    Create instance methods and validations specs on the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,29 @@
 require 'rails_helper'
 
-RSpec.decribe User, type: :model do
+RSpec.describe User, type: :model do
   describe 'validations' do
-    it {should validate_presence_of(:username)}
-    it {should validate_uniqueness_of(:username)}
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
+  end
+
+  describe 'instance methods' do
+    describe 'set_api_key' do
+      it 'sets the api key for a user' do
+        user = User.create(email: 'whatever@example.com', password: 'password')
+
+        expect(user.api_key).to be_a(String)
+        expect(user.api_key.length).to eq(28)
+      end
+    end
+
+    describe 'generate_api_key' do
+      it 'generates a randomly secure api_key' do
+        user = User.create(email: 'whatever@example.com', password: 'password')
+
+        expect(user.generate_api_key).to be_a(String)
+        expect(user.generate_api_key.length).to eq(28)
+      end
+    end
   end
 end
