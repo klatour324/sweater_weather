b4caca56fa936d681684b24f21b43ce812acbab9
Katy La Tour <klatour324@gmail.com>
2021-04-25 18:03:35 -0500

    Add rendered error messages to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
 
   def error(message = "Invalid request error. Please check the request")
     render json: { error: message }, status: :bad_request
+  end
+
+  def invalid_record(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+
+  def record_not_found(exception)
+    render json: { error: exception.message }, status: :not_found
   end
 end
