a6165d212ad28a1b4d71f507cab80e8aa848e320
Katy La Tour <klatour324@gmail.com>
2021-04-24 21:05:10 -0500

    Add coordinate poros spec

---
+++ spec/poros/coordinate_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Coordinate do
+  describe 'happy path' do
+    it 'should build a Coordinate PORO based on input' do
+      VCR.use_cassette('get_mapquest_coordinates') do
+        location = 'denver,co'
+        map_data = MapquestService.find_location(location)[:results].first
+        coordinates = Coordinate.new(map_data)
+
+        expect(coordinates).to be_a(Coordinate)
+        expect(coordinates.lat).to be_a(Float)
+        expect(coordinates.long).to be_a(Float)
+      end
+    end
+  end
+end
