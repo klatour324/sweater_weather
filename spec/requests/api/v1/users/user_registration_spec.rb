2e85ff79ee29ae9a85aab851759d4d6491a053b8
Katy La Tour <klatour324@gmail.com>
2021-04-25 17:30:30 -0500

    Update user registration spec to have json parsed data to send back in the response

--- spec/requests/api/v1/users/user_registration_spec.rb
+++ spec/requests/api/v1/users/user_registration_spec.rb
@@ -13,23 +13,26 @@ RSpec.describe 'Users Registration' do
       post "/api/v1/users", headers: headers, params: user_request_body.to_json
 
       user = User.find_by(email: user_request_body[:email])
-      expect(response.content_type).to eq("application/json; charset=utf-8")
+
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.content_type).to eq("application/json")
       expect(response).to be_successful
       expect(response.status).to eq(201)
       expect(user.email).to eq(user_request_body[:email])
-      expect(response.body).to be_a(Hash)
-      expect(response.body).to have_key(:data)
-      expect(response.body[:data]).to be_a(Hash)
-      expect(response.body[:data]).to have_key(:type)
-      expect(response.body[:data][:type]).to be_a(String)
-      expect(response.body[:data]).to have_key(:id)
-      expect(response.body[:data][:id]).to be_a(String)
-      expect(response.body[:data]).to have_key(:attributes)
-      expect(response.body[:data][:attributes]).to be_a(Hash)
-      expect(response.body[:data][:attributes]).to have_key(:email)
-      expect(response.body[:data][:attributes][:email]).to be_a(String)
-      expect(response.body[:data][:attributes]).to have_key(:api_key)
-      expect(response.body[:data][:attributes][:email][:api_key]).to be_a(String)
+      expect(result).to be_a(Hash)
+      expect(result).to have_key(:data)
+      expect(result[:data]).to be_a(Hash)
+      expect(result[:data]).to have_key(:type)
+      expect(result[:data][:type]).to be_a(String)
+      expect(result[:data]).to have_key(:id)
+      expect(result[:data][:id]).to be_a(String)
+      expect(result[:data]).to have_key(:attributes)
+      expect(result[:data][:attributes]).to be_a(Hash)
+      expect(result[:data][:attributes]).to have_key(:email)
+      expect(result[:data][:attributes][:email]).to be_a(String)
+      expect(result[:data][:attributes]).to have_key(:api_key)
+      expect(result[:data][:attributes][:api_key]).to be_a(String)
     end
   end
 end
