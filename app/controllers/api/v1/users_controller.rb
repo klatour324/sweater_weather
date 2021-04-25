9da14ac49970501d0f031ea1690e910dbca572b2
Katy La Tour <klatour324@gmail.com>
2021-04-25 18:00:28 -0500

    Update create method with to validate params passed in with the request

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,25 @@
 class Api::V1::UsersController < ApplicationController
-  # before_action :authenticate
 
   def create
-    require "pry"; binding.pry
+    if !valid_params.all? { |key| params.has_key?(key) }
+      error = "Invalid request. Please try again."
+      render(error)
+    else
+      new_user = User.new(user_params)
+      if new_user.save
+        render json: UsersSerializer.new(new_user), status: :created
+      end
+    end
+  end
+
+  private
+
+  def valid_params
+    [:email, :password, :password_confirmation]
+  end
+
+  def user_params
+    params[:email] = params[:email].downcase
+    params.permit(:email, :password, :password_confirmation)
   end
 end
