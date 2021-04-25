e704365d92343fce11dffdadd01e75c53cecbd56
Katy La Tour <klatour324@gmail.com>
2021-04-24 21:07:04 -0500

    Add 400 response error messages for each given sad path

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::ForecastController < ApplicationController
   def index
+    return error("Invalid location parameter") if params[:location] == '' || params[:location].nil?
     location = params[:location]
     map_data = MapquestService.find_location(location)[:results].first
     coordinates = Coordinate.new(map_data)
+
+    return error("Invalid location parameter") if coordinates.lat == 39.390897 && coordinates.long == -99.066067
+
     weather = WeatherService.find_forecast_for_location(coordinates.lat, coordinates.long)
     forecast = Forecast.new(weather)
 
