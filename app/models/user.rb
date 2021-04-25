18f80c6dac24a82626fa239776fb799f1f518e44
Katy La Tour <klatour324@gmail.com>
2021-04-25 16:25:49 -0500

    Add set api_key and generate api_key methods for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,16 @@
 class User < ApplicationRecord
-  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true, on: :create
   validates_presence_of :password, require: true
 
   has_secure_password
 
-  # before_create do |user|
-  #   user.api_key = user.generate_api_key
-  # end
-
   before_create :set_api_key
-  def set_spi_key
+
+  def set_api_key
     self.api_key = generate_api_key
   end
 
   def generate_api_key
-    # loop do
-      SecureRandom.base64.tr('+/=', 'Qrt')
-      # break token unless User.exists?(api_key: token)
-    # end
+    SecureRandom.hex(14)
   end
 end
