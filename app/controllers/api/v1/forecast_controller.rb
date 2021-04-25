9a9a6b162570395e550cbae6daa4f82f88489afe
Katy La Tour <klatour324@gmail.com>
2021-04-25 10:42:18 -0500

    Add conditional logic for sad path cases of user not passing in location correctly

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,15 +1,28 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    return error("Invalid location parameter") if params[:location] == '' || params[:location].nil?
+    return error("Invalid location parameter") if invalid_location?
     location = params[:location]
     map_data = MapquestService.find_location(location)[:results].first
     coordinates = Coordinate.new(map_data)
 
-    return error("Invalid location parameter") if coordinates.lat == 39.390897 && coordinates.long == -99.066067
+    return error("Invalid location parameter") if invalid_coordinates(coordinates)
 
     weather = WeatherService.find_forecast_for_location(coordinates.lat, coordinates.long)
     forecast = Forecast.new(weather)
 
     render json: ForecastSerializer.new(forecast)
+  end
+
+  private
+
+  def invalid_location?
+    return true if params[:location].nil?
+
+    city, state = params[:location].split(',')
+    params[:location] == '' || city.nil? || state.nil?
+  end
+
+  def invalid_coordinates(coordinates)
+    coordinates.lat == 39.390897 && coordinates.long == -99.066067
   end
 end
