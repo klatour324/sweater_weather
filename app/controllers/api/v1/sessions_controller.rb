98d498c62115e17452adba8ce82ea6cf56e605f5
Katy La Tour <klatour324@gmail.com>
2021-04-26 16:50:44 -0500

    Refactor create method with error message guard statement

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,15 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    if !valid_params.all? { |key| params.has_key?(key) }
-      error = "Password or email is incorrect."
+    error = "Password or email is incorrect."
+    return error(error) if !valid_params.all? { |key| params.has_key?(key) }
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      render json: UsersSerializer.new(user)
+    else
       error(error)
-    elsif
-      user = User.find_by(email: params[:email].downcase)
-      if user && user.authenticate(params[:password])
-        render json: UsersSerializer.new(user)
-      else
-        error(new_user.errors.full_messages.to_sentence)
-      end
     end
   end
 
