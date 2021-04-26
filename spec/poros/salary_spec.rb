ba63db88c6494cb6f6528c2e62f3a6bfcfa3ffaa
Katy La Tour <klatour324@gmail.com>
2021-04-26 12:58:03 -0500

    Update salary poro

--- spec/poros/salary_spec.rb
+++ spec/poros/salary_spec.rb
@@ -4,14 +4,11 @@ RSpec.describe Salary do
   describe 'happy path' do
     it 'should build a Salary PORO based on input' do
       location = 'chicago'
-      data = MapquestService.find_location(location)
-      latitude = data[:results].first[:locations].first[:latLng][:lat]
-      longitude = data[:results].first[:locations].first[:latLng][:lng]
-      weather = WeatherService.find_forecast_for_location(latitude, longitude)
       salary_data = TeleportService.find_salaries_for_location(location)
-      salary = Salary.new(salary_data, weather)
+      salary = Salary.new(salary_data, location)
 
       expect(salary).to be_a(Salary)
+      expect(salary.id).to be(nil)
       expect(salary.location).to be_a(String)
       expect(salary.forecast).to be_a(Hash)
       expect(salary.forecast).to have_key(:summary)
