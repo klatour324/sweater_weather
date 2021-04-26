a899b06b33a898e05c1068798a67c99b82fea830
Katy La Tour <klatour324@gmail.com>
2021-04-25 23:06:37 -0500

    Refactor forecast controller into a ForecastFacade

---
+++ app/facades/forecast_facade.rb
@@ -0,0 +1,19 @@
+class ForecastFacade
+  def self.get_forecast(location)
+    coordinates = get_coordinates(location)
+    weather = WeatherService.find_forecast_for_location(coordinates.lat, coordinates.long)
+    Forecast.new(weather)
+  end
+
+  def self.get_coordinates(location)
+    map_data = MapquestService.find_location(location)[:results].first
+    Coordinate.new(map_data)
+  end
+
+  def self.invalid_location?(params)
+    return true if params[:location].nil?
+
+    city, state = params[:location].split(',')
+    params[:location] == '' || city.nil? || state.nil?
+  end
+end
