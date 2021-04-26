5c4ca9c4a9a94af8b8de67729dc90c30591bbb8e
Katy La Tour <klatour324@gmail.com>
2021-04-26 12:04:37 -0500

    Render json serializer for Salaries

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,14 +1,8 @@
 class Api::V1::SalariesController < ApplicationController
   def index
     location = params[:destination]
-    def conn
-      Faraday.new(url: 'https://api.teleport.org')
-    end
+    salaries_data = TeleportService.find_salaries_for_location(location)
 
-    def find_salaries_for_location(location)
-      response = conn.get("api/urban_areas/#{location}/salaries/")
-
-      JSON.parse(response.body, symbolize_names: true)
-    end
+    render json: SalariesSerializer.new(salaries_data)
   end
 end
