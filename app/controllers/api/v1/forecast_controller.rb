450abecf1a1571cf7e2f105d99c4cfa27b64a89a
Katy La Tour <klatour324@gmail.com>
2021-04-25 23:07:05 -0500

    Refactor methods into Forecast Facade

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,28 +1,14 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    return error("Invalid location parameter") if invalid_location?
-    location = params[:location]
-    map_data = MapquestService.find_location(location)[:results].first
-    coordinates = Coordinate.new(map_data)
+    return error("Invalid location parameter") if ForecastFacade.invalid_location?(params)
+    weather = ForecastFacade.get_forecast(location)
 
-    return error("Invalid location parameter") if invalid_coordinates(coordinates)
-
-    weather = WeatherService.find_forecast_for_location(coordinates.lat, coordinates.long)
-    forecast = Forecast.new(weather)
-
-    render json: ForecastSerializer.new(forecast)
+    render json: ForecastSerializer.new(weather)
   end
 
   private
 
-  def invalid_location?
-    return true if params[:location].nil?
-
-    city, state = params[:location].split(',')
-    params[:location] == '' || city.nil? || state.nil?
-  end
-
-  def invalid_coordinates(coordinates)
-    coordinates.lat == 39.390897 && coordinates.long == -99.066067
+  def location
+    params[:location]
   end
 end
