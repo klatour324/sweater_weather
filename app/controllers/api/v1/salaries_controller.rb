39eeda87965cd159a29c350d834417237cd70d93
Katy La Tour <klatour324@gmail.com>
2021-04-26 10:55:13 -0500

    Create api call in controller to start

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::SalariesController < ApplicationController
   def index
+    location = params[:destination]
+    def conn
+      Faraday.new(url: 'https://api.teleport.org')
+    end
+
+    def find_salaries_for_location(location)
+      response = conn.get("api/urban_areas/#{location}/salaries/")
+
+      JSON.parse(response.body, symbolize_names: true)
+    end
   end
 end
