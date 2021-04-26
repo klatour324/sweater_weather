941acf3490c440495329caa707400bcd27657963
Katy La Tour <klatour324@gmail.com>
2021-04-26 12:03:51 -0500

    Continue building out expectations

--- spec/requests/api/v1/salaries/salaries_for_destination_spec.rb
+++ spec/requests/api/v1/salaries/salaries_for_destination_spec.rb
@@ -10,6 +10,20 @@ RSpec.describe 'Salaries for Destination' do
 
       expect(response).to be_successful
       expect(response.status).to eq(200)
+
+      expect(result).to be_a(Hash)
+      expect(result).to have_key([:data])
+      expect(result[:data]).to be_a(Hash)
+      expect(result[:data].count).to eq(3)
+      expect(result[:data][:id]).to eq(nil)
+      expect(result[:data]).to have_key(:type)
+      expect(result[:data][:type]).to eq('salaries')
+      expect(result[:data]).to have_key(:attributes)
+      expect(result[:data][:attributes].count).to eq(4)
+      expect(result[:data][:attributes]).to be_a(Hash)
+      expect(result[:data][:attributes]).to have_key(:destionation)
+      expect(result[:data][:attributes][:destination]).to be_a(String)
+      expect(result[:data][:attributes]).to have_key(:forecast)
     end
   end
 end
