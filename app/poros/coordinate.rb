80c2e9bbfddb5f809be7d06b9c77d9c7a5cb7ca4
Katy La Tour <klatour324@gmail.com>
2021-04-26 18:42:50 -0500

    add logic to coordinates poro to handle coordinates for road trip

--- app/poros/coordinate.rb
+++ app/poros/coordinate.rb
@@ -2,7 +2,23 @@ class Coordinate
   attr_reader :lat,
               :long
   def initialize(data)
-    @lat = data[:locations].first[:latLng][:lat]
-    @long = data[:locations].first[:latLng][:lng]
+    @lat = find_lat(data)
+    @long = find_lng(data)
+  end
+
+  def find_lat(data)
+    if data[:results].first[:locations].empty?
+      0
+    else
+      data[:locations].first[:latLng][:lat]
+    end
+  end
+
+  def find_long(data)
+    if data[:results].first[:locations].empty?
+      0
+    else
+      data[:locations].first[:latLng][:lng]
+    end
   end
 end
