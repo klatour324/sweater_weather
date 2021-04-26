a9fb72b1f634339031fce2848fbaba1fd3813e9b
Katy La Tour <klatour324@gmail.com>
2021-04-26 16:20:44 -0500

    Add create action for sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    if !valid_params.all? { |key| params.has_key?(key) }
+      error = "Invalid request. Please try again."
+      error(error)
+    elsif
+      user = User.find_by(email: params[:email].downcase)
+      if user && user.authenticate(params[:password])
+        render json: UsersSerializer.new(user)
+      else
+        error(new_user.errors.full_messages.to_sentence)
+      end
+    end
+  end
+
+  private
+
+  def valid_params
+    [:email, :password]
+  end
+end
