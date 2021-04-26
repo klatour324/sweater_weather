171ac17597ea8dc975790cc1dbd0cc8d495d6ace
Katy La Tour <klatour324@gmail.com>
2021-04-26 12:26:24 -0500

    Begin building out poro for salary

--- app/poros/salary.rb
+++ app/poros/salary.rb
@@ -1,2 +1,16 @@
 class Salary
+    attr_reader :destination,
+                :forecast,
+                :salaries
+
+
+    def initialize(data)
+      @destination = data[:destination]
+      @forecast = CurrentWeather.new(data[:current])
+      @salaries = data[:salaries]
+    end
+
+    # def tech_salaries(data)
+    #
+    # end
 end
