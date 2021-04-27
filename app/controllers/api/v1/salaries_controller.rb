5678c5f19493d8b4b46b89fdbbf0b1df3fb1ec0b
Katy La Tour <klatour324@gmail.com>
2021-04-27 09:43:14 -0500

    Add notes for implementing refactor process

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::SalariesController < ApplicationController
   def index
     location = params[:destination]
     salaries_data = TeleportService.find_salaries_for_location(location)
-
+    # need to add Salary instance here
+    # refactor this controller into a Facade
     render json: SalariesSerializer.new(salaries_data)
   end
 end
