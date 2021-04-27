82d337a3fbcef72ccc5d42eba547db11fb169d14
Katy La Tour <klatour324@gmail.com>
2021-04-27 13:54:20 -0500

    Update create action with road trip logic

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
+    user = User.find_by!(api_key: params[:api_key])
+    start = params[:origin]
+    destination = params[:destination]
+    trip_data = MapquestService.get_directions(start, destination)
+    forecast = ForecastFacade.get_forecast(destination)
+    road_trip = RoadTrip.new(start, destination, trip_data, forecast)
+
+    render json: RoadtripSerializer.new(road_trip)
   end
 end
