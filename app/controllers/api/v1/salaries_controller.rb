c4b92db8621292bf437ff2a356a5ae29f5dcc81e
Katy La Tour <klatour324@gmail.com>
2021-04-27 10:38:02 -0500

    Refactor out logic in index method for forecast search data and salary search data into SlariesFacade

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::SalariesController < ApplicationController
   def index
-    location = params[:destination]
-    coordinates = MapquestService.find_location(location)
-    coordinates = coordinates[:results].first[:locations].first[:latLng]
-    forecast = WeatherService.find_forecast_for_location(coordinates)
-    salaries_data = TeleportService.find_salaries_for_location(location)
-    salary = Salary.new(location, salaries_data, forecast_data)
-    # need to add Salary instance here
-    # refactor this controller into a Facade
-    render json: SalariesSerializer.new(salary)
+    # location = params[:destination]
+    # coordinates = MapquestService.find_location(location)
+    # coordinates = coordinates[:results].first[:locations].first[:latLng]
+    # forecast = WeatherService.find_forecast_for_location(coordinates)
+    # salaries_data = TeleportService.find_salaries_for_location(location)
+    # salary = Salary.new(location, salaries_data, forecast_data)
+    salaries = SalariesFacade.search(params[:destination])
+
+    render json: SalariesSerializer.new(salaries)
   end
 end
