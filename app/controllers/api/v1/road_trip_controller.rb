f8768411cd76319b009fe1e2ad536710ccf56409
Katy La Tour <klatour324@gmail.com>
2021-04-27 17:55:38 -0500

    Refactor controller methods

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -4,7 +4,8 @@ class Api::V1::RoadTripController < ApplicationController
     start = params[:origin]
     destination = params[:destination]
     trip_data = MapquestService.get_directions(start, destination)
-    forecast = ForecastFacade.get_forecast(destination)
+    coordinates = Coordinate.new(trip_data[:route][:boundingBox])
+    forecast = WeatherService.find_forecast_for_location(coordinates.lat, coordinates.lng)
     road_trip = RoadTrip.new(start, destination, trip_data, forecast)
 
     render json: RoadtripSerializer.new(road_trip)
