f64f982fc70672bd0b2744fcf2813855d582d71d
Katy La Tour <klatour324@gmail.com>
2021-04-27 10:55:36 -0500

    Remove commented out lines

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,11 +1,5 @@
 class Api::V1::SalariesController < ApplicationController
   def index
-    # location = params[:destination]
-    # coordinates = MapquestService.find_location(location)
-    # coordinates = coordinates[:results].first[:locations].first[:latLng]
-    # forecast = WeatherService.find_forecast_for_location(coordinates)
-    # salaries_data = TeleportService.find_salaries_for_location(location)
-    # salary = Salary.new(location, salaries_data, forecast_data)
     return error("Invalid destination parameter") if invalid_destination?(params)
     salaries = SalariesFacade.search(params[:destination])
 
