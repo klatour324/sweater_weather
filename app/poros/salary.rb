9c355e7381d561cdb3ad0dbc73fbd2526bcc474e
Katy La Tour <klatour324@gmail.com>
2021-04-27 10:00:20 -0500

    Update salaries method. Fix poros spec to reflect necessary data for response

--- app/poros/salary.rb
+++ app/poros/salary.rb
@@ -2,23 +2,31 @@ class Salary
     attr_reader :id,
                 :destination
 
-    def initialize(destination, salary_data, forecast_data)
+    def initialize(destination, salaries_data, forecast_data)
       @id = id
       @destination = destination
+      @salaries_data = salaries_data
+      @forecast_data = forecast_data
     end
 
-    def weather_forecast(destination)
-      coordinates = MapquestService.find_location(destination)
-      coordinates = coordinates[:results].first[:locations].first[:latLng]
-      forecast = WeatherService.find_forecast_for_location(coordinates)
-    end
+    # def forecast(destination)
+    #   coordinates = MapquestService.find_location(destination)
+    #   coordinates = coordinates[:results].first[:locations].first[:latLng]
+    #   forecast = WeatherService.find_forecast_for_location(coordinates)
+    # end
 
-    def salaries(destination)
-      salaries = TeleportService.find_salaries(destination.downcase)
-    job_titles = ["Data Analyst","Data Scientist","Mobile Developer",
-            "QA Engineer","Software Engineer","Systems Administrator","Web Developer"]
-    job_titles.map do |job_title|
-      # require "pry"; binding.pry
+    def salaries
+      salaries = TeleportService.find_salaries_for_location(destination.downcase)
+    job_titles = [
+                  "Data Analyst","Data Scientist","Mobile Developer",
+                  "QA Engineer","Software Engineer",
+                  "Systems Administrator","Web Developer"
+                 ]
+    tech_jobs = @salaries_data[:salaries].find_all do |info|
+      job_titles.include?(info[:job][:title])
+    end
+    tech_jobs.map do |job|
+      require "pry"; binding.pry
     end
   end
 end
