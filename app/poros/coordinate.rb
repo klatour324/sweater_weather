755c0a57111d5b353ca998c2deb5490be9ade8d9
Katy La Tour <klatour324@gmail.com>
2021-04-27 13:53:05 -0500

    Fix helper method logic to mitigate errors

--- app/poros/coordinate.rb
+++ app/poros/coordinate.rb
@@ -1,24 +1,17 @@
 class Coordinate
   attr_reader :lat,
               :long
+
   def initialize(data)
     @lat = find_lat(data)
     @long = find_lng(data)
   end
 
   def find_lat(data)
-    if data[:results].first[:locations].empty?
-      0
-    else
-      data[:locations].first[:latLng][:lat]
-    end
+    data[:locations].first[:latLng][:lat]
   end
 
-  def find_long(data)
-    if data[:results].first[:locations].empty?
-      0
-    else
-      data[:locations].first[:latLng][:lng]
-    end
+  def find_lng(data)
+    data[:locations].first[:latLng][:lng]
   end
 end
