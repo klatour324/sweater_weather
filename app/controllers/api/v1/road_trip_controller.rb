54979ce2f0a7165e7936bb4b73df607b81713c3b
Katy La Tour <klatour324@gmail.com>
2021-04-28 09:03:22 -0500

    Refactor rodatrip controller into facade. Remove sessions facade commented out lines

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,14 +2,6 @@ class Api::V1::RoadTripController < ApplicationController
   def create
     return error("Invalid parameters") if !params[:origin] || !params[:destination]
     user = User.find_by!(api_key: params[:api_key])
-    # start = params[:origin]
-    # destination = params[:destination]
-    # road_trip = RoadTripFacade.create_new_road_trip(params)
-
-    # trip_data = MapquestService.get_directions(start, destination)
-    # coordinates = Coordinate.new(trip_data[:route][:boundingBox])
-    # forecast = WeatherService.find_forecast_for_location(coordinates.lat, coordinates.lng)
-    # road_trip = RoadTrip.new(start, destination, trip_data, forecast)
     road_trip = RoadTripFacade.create_new_road_trip(params[:origin], params[:destination])
 
     render json: RoadtripSerializer.new(road_trip)
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,10 +3,8 @@ class Api::V1::SessionsController < ApplicationController
     error = "Password or email is incorrect."
     return error(error) if !valid_params.all? { |key| params.has_key?(key) }
     user = User.find_by(email: params[:email].downcase)
+
     if user && user.authenticate(params[:password])
-    # user = SessionsFacade.get_authenticated_user(params)
-    # if user == 'invalid parameters'
-    # elsif user
       render json: UsersSerializer.new(user)
     else
       error(error)
