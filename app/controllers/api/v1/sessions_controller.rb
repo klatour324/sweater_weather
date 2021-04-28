2c89ab7d28fa875c4b59a3b79e53aea3ff28910d
Katy La Tour <klatour324@gmail.com>
2021-04-28 01:42:04 -0500

    Refactor sessions controller into sessions facade

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,18 +1,13 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    error = "Password or email is incorrect."
-    return error(error) if !valid_params.all? { |key| params.has_key?(key) }
-    user = User.find_by(email: params[:email].downcase)
-    if user && user.authenticate(params[:password])
+    user = SessionsFacade.get_authenticated_user(valid_params)
+    if user == 'invalid parameters'
+      error = "Password or email is incorrect."
+      return error(error)
+    elsif user
       render json: UsersSerializer.new(user)
     else
       error(error)
     end
-  end
-
-  private
-
-  def valid_params
-    [:email, :password]
   end
 end
---
+++ spec/facades/sessions_facade_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe SessionsFacade do
+  describe ' happy path' do
+    it 'can get the User for a given session with valid parameters' do
+      user = User.create!(email: 'whatever@example.com', password: 'password', password_confirmation: 'password')
+      params = {email: user.email, password: 'password'}
+
+      result = SessionsFacade.get_authenticated_user(params)
+
+      expect(result).to be_a(User)
+      expect(result).to eq(user)
+    end
+  end
+end
